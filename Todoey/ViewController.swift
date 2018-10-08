227adca52ad948462909eec9f7973f72d502f92a
PINNOCK, MICHELLE <mpinnock@ny-39721-lt.local>
2018-10-08 08:59:49 -0400

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by PINNOCK, MICHELLE on 10/8/18.
+//  Copyright © 2018 PINNOCK, MICHELLE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

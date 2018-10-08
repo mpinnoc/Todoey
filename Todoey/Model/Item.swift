ac3b40aa421e0927bd6bf365ab3d385203c9afd4
PINNOCK, MICHELLE <mpinnock@ny-39721-lt.local>
2018-10-08 11:26:26 -0400

    Convert to MVC Design

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by PINNOCK, MICHELLE on 10/8/18.
+//  Copyright © 2018 PINNOCK, MICHELLE. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+    
+}
